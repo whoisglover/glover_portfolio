59e87e0959f8f71134e1d75c47fb5f223a9038a1
Danny Glover <djglover00@gmail.com>
2014-11-22 22:12:46 -0800

    adding random greeting and action to landing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def index
     @time = Time.now
+    hellos = ["Hello", "Bonjour", "Cześć", "Ciao", "Hola", "你好", "Aloha", "Salut", "Hi", "Hi there", "Hello"]
+    actions = ["let's get building", "let's get moving", "let's learn together", "let's change the world", "let's build something beautiful", "let's make something meaningful", "let's shake things up"]
+    @greeting = hellos[rand(hellos.length)]
+    @action = actions[rand(actions.length)]
   end
 end
