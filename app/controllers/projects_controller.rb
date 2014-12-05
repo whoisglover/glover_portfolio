bc2816513f81b4ba6c7f69969e8831f7cdaacf55
Danny Glover <djglover00@gmail.com>
2014-12-05 17:06:20 -0600

    creating single pages for projects: frogpress capms and when-where done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,21 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.friendly.find(params[:id])
-    render :freeteeth
+    p "!" * 100
+    p @project.name
+    case @project.slug
+    when "frogpress"
+      render :frogpress
+    when "capms"
+      render :capms
+    when "when-where"
+      render :whenwhere
+    when "fit-together"
+      render :fittogether
+    when "wow-stats-realms"
+      render :wowstatsrealms
+    else
+      redirect_to("/")
+    end
   end
 end
